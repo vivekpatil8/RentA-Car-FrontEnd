4cec2e383f71797074e9f6086aabc3484fbe7b8c
vivek.patil <vivek.patil@yanitsolutions.com>
2022-11-29 15:28:28 +0530

    new

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'Trax.Flagship.Inspect',
+  appName: 'Flagship.Inspect',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
